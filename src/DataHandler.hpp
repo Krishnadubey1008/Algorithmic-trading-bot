e48188683f8341097f2d5b515d7870e6e346dac0
Krishna Dubey <121748506+Krishnadubey1008@users.noreply.github.com>
2025-08-06 06:05:55 +0000

    Update DataHandler.hpp

--- src/DataHandler.hpp
+++ src/DataHandler.hpp
@@ -12,3 +12,9 @@ struct Bar {
     double close;
     long volume;
 };
+// Abstract base class for all data handlers
+class DataHandler {
+public:
+    virtual ~DataHandler() = default;
+    virtual bool get_next_bar(Bar& bar) = 0;
+};
