90a9262b570f93b059843d2e4fc9c699bb590c0d
Krishna Dubey <121748506+Krishnadubey1008@users.noreply.github.com>
2025-08-06 06:16:27 +0000

    Update config.h

--- src/config.h
+++ src/config.h
@@ -4,9 +4,9 @@
 
 namespace config {
     // --- API Keys ---
-    const std::string ALPHA_VANTAGE_API_KEY = "YOUR_ALPHA_VANTAGE_API_KEY";
-    const std::string ALPACA_API_KEY        = "YOUR_ALPACA_API_KEY";
-    const std::string ALPACA_SECRET_KEY     = "YOUR_ALPACA_SECRET_KEY";
+    const std::string ALPHA_VANTAGE_API_KEY = "U99C1ZPRB2E673SR";
+    const std::string ALPACA_API_KEY        = "PKTF90X4J9FKS6IOZ4NB";
+    const std::string ALPACA_SECRET_KEY     = "AbGsKRGrgdMMaNyTF4yox6XZUat8Q6l1l6C4g1Bb";
 
     // --- Single-Asset Strategy Parameters ---
     const std::string SYMBOL = "AAPL";
